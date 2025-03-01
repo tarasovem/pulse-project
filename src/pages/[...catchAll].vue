c2bf346712d773fe1161e523db98a69ae17a0784
e.tarasov <tarasovem@gmail.com>
2025-03-01 18:17:08 +0300

    feat: меняет роутинг на file-based

---
+++ src/pages/[...catchAll].vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p>404 Not Found</p>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,9 @@
-import { h } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: import('@/views/SingleProjectView.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color: red; font-size: 2em;' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router
