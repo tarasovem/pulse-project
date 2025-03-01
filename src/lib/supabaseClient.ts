70ce50d9523bfb83cea893f971519321709439ae
e.tarasov <tarasovem@gmail.com>
2025-03-01 21:23:44 +0300

    feat: добавляет файлы для работы с чувствительными данными настроек

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
+)
