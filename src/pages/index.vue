9b7051547afb001e35dafc42331d02a9a6b57cb1
e.tarasov <tarasovem@gmail.com>
2025-03-02 17:53:04 +0300

    feat: добавляет подгрузку данных из базы при запуске приложения

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  console.log('Projects: ', data)
+})()
+</script>
 
 <template>
   <div>
