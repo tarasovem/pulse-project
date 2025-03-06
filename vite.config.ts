c3f147928da8262201b87bc0de919b7bff9e7019
e.tarasov <tarasovem@gmail.com>
2025-03-06 16:56:52 +0300

    feat: подключает tailwindcss и shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
+
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+       plugins: [tailwind(), autoprefixer()],
+    }
+  },
   plugins: [VueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
